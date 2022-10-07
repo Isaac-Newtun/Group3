7ada1600147e6e1358a74e8a7638508981c1f66c
Isaac-Newtun <115239223+Isaac-Newtun@users.noreply.github.com>
2022-10-07 19:48:30 +0300

    Please upload the files
    Thank you

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("button2").onclick = function(){
+    var name = window.prompt("Pangalan: ");
+
+    window.alert("Kamusta " + name + "!")
+
+    console.log("Website Viewer(s) Name = " + name);
+}
+
---
+++ members.js
@@ -0,0 +1,3 @@
+window.alert("Kamusta at magandang araw sayo!");
+
+window.alert("Dito niyo makikita ang mga members ng group 3!");
